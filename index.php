5b8dbcdffa56a28a4e94cd997e5267063da2c6b0
Pierre-Luc Brault <plbrault@Pierre-Lucs-MacBook-Pro.local>
2020-04-09 20:27:37 -0400

    Initial Files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+<style>
+  html {
+    background-color: black;
+    color: white;
+    width: 100%;
+    height: 100%;
+  }
+
+  h1 {
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    top: 45%;
+  }
+</style>
+
+<h1><?= bloginfo('name') ?></h1>
